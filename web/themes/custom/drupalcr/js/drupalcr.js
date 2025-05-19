e5fbb2a3929480d9c2c24f022938edac5e4e9ba2
Alejandro Madrigal <me@alemadlei.tech>
2025-05-19 11:36:07 -0600

    Tema base.

---
+++ web/themes/custom/drupalcr/js/drupalcr.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * DrupalCR behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalcr = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
